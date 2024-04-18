e259256605c95339c0f484587f90bcd9be6851aa
soraqah <soraqah@gmailcom>
2024-04-18 09:12:17 +0300

    I style work on js file

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const display=document.getElementById("display")
+
+function appendToDisplay(input){
+    display.value =+ input;
+
+}
+function clearDisplay(){
+    display.value= " ";
+
+}
+
+function calculate(){
+    let first_value=display.value;
+    let oprator=document.getElementsByClassName("opretor_but");
+    
+    
+}
